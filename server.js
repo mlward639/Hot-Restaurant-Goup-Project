c78ba3d54f2b88cec38ce91053392a187985ee36
Maggie Ward <mlward639@gmail.com>
2021-07-14 13:49:57 -0400

    pull

--- server.js
+++ server.js
@@ -7,29 +7,54 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.get('/', (req,res) => res.send("I work!"));
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, './Home.html')));    //HOME PAGE
+app.get('/Make', (req,res) => res.sendFile(path.join(__dirname, './Make.html')));   //ADD A RESERVATION PAGE
+app.get('/View', (req,res) => res.sendFile(path.join(__dirname, './View.html')));  //VIEW RESERVATIONS AND WAITLIST
 
-app.get('/Home', (req,res) => res.sendFile(__dirname, './Home.html'));    //HOME PAGE
-app.get('/Make', (req,res) => res.sendFile(__dirname, './Make.html'));   //ADD A RESERVATION PAGE
-app.get('/View', (req,res) => res.sendFile(__dirname, './View.html'));  //VIEW RESERVATIONS AND WAITLIST
 
+const reservations = [
+    {
+        routeName: '',
+        name: '',
+        phone: '',
+        email: '',
+        uID: '',
+    },
+];
+
+const waitList = [
+    {
+        name: '',
+        phone: '',
+        email: '',
+        uID: '',
+    },
+];
 
 //this is for getting reservations
 //should pull from reservation Array
 //DO THE SAME WITH WAITLIST 
 //use star wars as reference
 
-app.get('/api/reservations/:reservation', (req, res) => {
+app.get('/api/reservations', (req, res) => res.json(reservations));
 
-})
+app.get('/api/waitList', (req, res) => res.json(waitList));
+
+// if(reservations.length < 5){
+// app.post('/api/reservations', (req, res) )
+//     const newReservation = req.body;
+
+//     newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
+//     console.log(newReservation);
+// }
+// else
+// app.post('/api/waitlist', (req,res) =>  {
+
+// })
 
 
 
-
-const reservations = [];
- 
-const waitList = [];
 
 //res.sendFile(path.join(__dirname,'home.html')));
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));g
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
