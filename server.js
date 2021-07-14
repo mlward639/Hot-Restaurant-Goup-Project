97b84db6032ea2d305304202dab368a281b453fa
Austin Huffman <austinhuffman0@gmail.com>
2021-07-14 13:49:32 -0400

    Post for reservations working.

--- server.js
+++ server.js
@@ -11,25 +11,25 @@ app.get('/', (req,res) => res.sendFile(path.join(__dirname, './Home.html')));   
 app.get('/Make', (req,res) => res.sendFile(path.join(__dirname, './Make.html')));   //ADD A RESERVATION PAGE
 app.get('/View', (req,res) => res.sendFile(path.join(__dirname, './View.html')));  //VIEW RESERVATIONS AND WAITLIST
 
+const reservations = [];
 
-const reservations = [
-    {
-        routeName: '',
-        name: '',
-        phone: '',
-        email: '',
-        uID: '',
-    },
-];
+//example reservation
+    // {
+    //     routeName: '',
+    //     name: '',
+    //     phone: '',
+    //     email: '',
+    //     uID: '',
+    // },
 
-const waitList = [
-    {
-        name: '',
-        phone: '',
-        email: '',
-        uID: '',
-    },
-];
+const waitList = [];
+
+    // {
+    //     name: '',
+    //     phone: '',
+    //     email: '',
+    //     uID: '',
+    // },
 
 //this is for getting reservations
 //should pull from reservation Array
@@ -40,21 +40,25 @@ app.get('/api/reservations', (req, res) => res.json(reservations));
 
 app.get('/api/waitList', (req, res) => res.json(waitList));
 
-// if(reservations.length < 5){
-// app.post('/api/reservations', (req, res) )
-//     const newReservation = req.body;
+if(reservations.length < 5){
+app.post('/api/reservations', (req, res) => {
+    const newReservation = req.body;
 
-//     newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
-//     console.log(newReservation);
-// }
+    newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newReservation);
+    reservations.push(newReservation);
+    res.json(newReservation);
+});
+}
+
 // else
 // app.post('/api/waitlist', (req,res) =>  {
 
-// })
+// // })
 
 
 
 
-//res.sendFile(path.join(__dirname,'home.html')));
+// //res.sendFile(path.join(__dirname,'home.html')));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
