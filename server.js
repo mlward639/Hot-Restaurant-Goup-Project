0e642b2e74c963faf93dc50e1118b04351784632
Austin Huffman <austinhuffman0@gmail.com>
2021-07-14 13:16:21 -0400

    Routs for redirecting annd set a get route for reservations

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ app.use(express.json());
 
 app.get('/', (req,res) => res.send("I work!"));
 
+app.get('/Home', (req,res) => res.sendFile(__dirname, './Home.html'));    //HOME PAGE
+app.get('/Make', (req,res) => res.sendFile(__dirname, './Make.html'));   //ADD A RESERVATION PAGE
+app.get('/View', (req,res) => res.sendFile(__dirname, './View.html'));  //VIEW RESERVATIONS AND WAITLIST
+
+
+//this is for getting reservations
+//should pull from reservation Array
+//DO THE SAME WITH WAITLIST 
+//use star wars as reference
+
+app.get('/api/reservations/:reservation', (req, res) => {
+
+})
+
+
+
+
+const reservations = [];
+ 
+const waitList = [];
+
 //res.sendFile(path.join(__dirname,'home.html')));
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));g
